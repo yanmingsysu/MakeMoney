d5a0d551b37d6a0d34d666705317e31a36129c84
yanmingsysu <yanming.sysu@bytedance.com>
2019-06-29 15:40:55 +0800

    project init

---
+++ MakeMoney/MakeMoney/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MakeMoney
+//
+//  Created by yanming.sysu on 2019/6/29.
+//  Copyright © 2019 yanming.sysu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
